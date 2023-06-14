24f868a80ad1b1a5284e42f792599de1952f561b
FunctionJoo <joojy0914@gmail.com>
2023-06-14 14:20:58 +0900

    라이브러리 관리좀 해야하는데 실프로젝트 끝나면 다시 정리 해보겠음

--- wbz-popup.v1.js
+++ wbz-popup.v1.js
@@ -1,6 +1,7 @@
 const $popup = {
 	// 변수
 	// z-index 조절 필요시 값 변경
+	scrollY: 0,
 	zIndex: 10000,
 	count: 0,
 	activePopup: [],
@@ -10,6 +11,13 @@ const $popup = {
 		if (this.count == 0) {
 			document.body.insertAdjacentHTML('beforeend', this.$popbg);
 			document.querySelector('#wbz-popup-bg').style.zIndex = this.zIndex;
+
+			this.scrollY = window.pageYOffset;
+			const body = document.querySelector('body');
+			body.style.overflow = 'hidden';
+			body.style.position = 'fixed';
+			body.style.top = `-${this.scrollY}px`;
+			body.style.width = '100%';
 		}
 		this.count++;
 	},
@@ -23,8 +31,22 @@ const $popup = {
 		}
 		// 타이틀 입력되지 않은 경우 빈값 처리
 		if (!opt.title) {
-			opt.title = '';
+			opt.title = '&nbsp;';
 		}
+
+		// confirm 타입인 경우
+		let buttonTemplate;
+		if (opt.isConfirm == true) {
+			buttonTemplate = `
+			<button type="button" class="popup-close popup-close-event">취소</button>
+			<button type="button" class="popup-confirm-event">확인</button>
+			`;
+		} else {
+			buttonTemplate = `
+			<button type="button" class="popup-close popup-close-event">확인</button>
+			`;
+		}
+
 		let temp = `
 			<div class="wbz-popup-cont is-instant on" id="wbz-popup-${this.zIndex + this.count}">
 				<div class="popup-header">
@@ -36,7 +58,7 @@ const $popup = {
 						${opt.content}
 					</div>
 					<div class="popup-button">
-						<button type="button" class="popup-close popup-close-event">확인</button>
+						${buttonTemplate}
 					</div>
 				</div>
 			</div>
@@ -45,40 +67,63 @@ const $popup = {
 		this.activePopup.push(`#wbz-popup-${this.zIndex + this.count}`);
 
 		// 이벤트 설정
-		if (opt.closeEvent) {
-			// 설정한 이벤트가 있는경우 해당 이벤트를 실행
-			document.querySelectorAll(`#wbz-popup-${this.zIndex + this.count} .popup-close`).forEach(($close) => {
-				$close.addEventListener('click', () => {
+		if (opt.isConfirm == true) {
+			if (opt.confirmEvent) {
+				document.querySelector(`#wbz-popup-${this.zIndex + this.count} .popup-confirm-event`).addEventListener('click', () => {
+					opt.confirmEvent();
+				});
+			} else if (opt.closeEvent) {
+				document.querySelector(`#wbz-popup-${this.zIndex + this.count} .popup-confirm-event`).addEventListener('click', () => {
 					opt.closeEvent();
 				});
-			});
-		} else {
-			// 설정한 이벤트가 없는경우 닫기
-			document.querySelectorAll(`#wbz-popup-${this.zIndex + this.count} .popup-close`).forEach(($close) => {
-				$close.addEventListener('click', () => {
+			} else {
+				document.querySelector(`#wbz-popup-${this.zIndex + this.count} .popup-confirm-event`).addEventListener('click', () => {
 					this.close();
 				});
-			});
+			}
 		}
+	},
+	confirm: function(opt) {
+		opt.isConfirm = true;
+		this.alert(opt);
 	},
 	template: function(target) {
 		this.backgroundSetting();
 		document.querySelector(target).classList.add('on');
+		if (document.querySelector(target).classList.contains('type-bottom')) {
+			setTimeout(function() {document.querySelector(target).classList.add('ani');}, 10);
+		}
 		document.querySelector(target).style.zIndex = this.zIndex + this.count;
 		this.activePopup.push(target);
+
+		document.querySelector('#wbz-popup-bg').addEventListener('click', function() {
+			$popup.closeAll();
+		})
 	},
 	close: function() {
 		let $activePopup = document.querySelector(this.activePopup[this.activePopup.length - 1]);
 		if ($activePopup) {
 			if (this.count == 1) {
 				document.querySelector('#wbz-popup-bg').remove();
+
+				const body = document.querySelector('body');
+				body.style.removeProperty('overflow');
+				body.style.removeProperty('position');
+				body.style.removeProperty('top');
+				body.style.removeProperty('width');
+				window.scrollTo(0, this.scrollY);
 			}
 			if ($activePopup.classList.contains('is-instant')) {
 				// 인스턴트 팝업인 경우
 				$activePopup.remove();
 			} else {
 				// 템플릿 팝업의 경우
-				$activePopup.classList.remove('on');
+				if ($activePopup.classList.contains('type-bottom')) {
+					$activePopup.classList.remove('ani');
+					setTimeout(function() {$activePopup.classList.remove('on');}, 400);
+				} else {
+					$activePopup.classList.remove('on');
+				}
 			}
 			this.activePopup.pop();
 			this.count--;
@@ -89,15 +134,17 @@ const $popup = {
 		}
 	},
 	closeAll: function() {
-		document.querySelector('#wbz-popup-bg').remove();
-		document.querySelectorAll('.wbz-popup-cont').forEach(($el) => {
-			if ($el.classList.contains('is-instant')) {
-				$el.remove();
-			} else {
-				$el.classList.remove('on');
-			}
+		this.activePopup.forEach(() => {
+			this.close();
 		});
 		this.activePopup = [];
 		this.count = 0;
 	}
 }
+
+// 팝업 이벤트 리스너 등록
+window.addEventListener('DOMContentLoaded', () => {
+	document.querySelectorAll('[data-popup^="#"][data-popup$="-pop"]').forEach(element => {
+		element.addEventListener('click', () => $popup.template(element.dataset.popup));
+	});
+});
